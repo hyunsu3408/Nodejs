0ae29c0b111ee3750ed18182beac4e5c772c58d0
hyunsu3408 <hyunsu3408@gmail.com>
2025-06-16 00:51:56 +0900

    f

--- function.js
+++ function.js
@@ -19,6 +19,8 @@ let hi = function(){
     return '안녕하세요?'
 }
 
+let sum = (a,b) => a+b;
+
 // 방법5
 // 돌려줄 문장이 한줄이라면 return과 {} 생략 가능
 let bye = () => "메롱"
@@ -32,3 +34,4 @@ greet('현수')
 greeting('김현수')
 console.log(hi())
 console.log(bye())
+console.log(sum(101,20))
