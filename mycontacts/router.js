b873b72a3947729510a6565a3e5b805c37080786
hyunsu3408 <hyunsu3408@gmail.com>
2025-06-18 23:56:04 +0900

    app

---
+++ mycontacts/router.js
@@ -0,0 +1,26 @@
+
+const express = require('express');
+const app = express();
+
+const router = express.Router();
+
+router.route("/contacts")
+.get((req, res) => {
+    res.send("Contacts Page");
+})
+.post((req, res) => {
+    res.send("Create Contacts");
+});
+
+router.route("/contacts/:id")
+.put((req, res) => {
+    res.send(`update contact for id ${req.params.id}`);
+})
+.delete((req, res) => {
+    res.send(`delete contact for id ${req.params.id}`);
+});
+
+app.use('/',router);
+app.listen(3500, ()=>{
+    console.log("서버 실행 중");
+})
