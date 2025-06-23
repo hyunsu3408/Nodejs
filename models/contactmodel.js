4c52efe1f2f7886a9fc4adea48ecbbbd7ed15b59
hyunsu3408 <hyunsu3408@gmail.com>
2025-06-23 23:18:57 +0900

    m

--- models/contactmodel.js
+++ models/contactmodel.js
@@ -17,9 +17,11 @@ const contactSchema = new mongoose.Schema({
 {
     timestamps:true
 })
+// time stamps -> true 하면 자동으로 시간기록이됨
 
 // 스키마 -> 모델
 // mongoose.model(모델명, 스키마명)
 const Contact = mongoose.model("Contact",contactSchema)
 
+// 모델 내보내기
 model.exports = Contact;
